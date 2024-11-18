b339564ae5890eec380c41db35a04cb5b3937f41
devCsaba <134238949+devCsaba@users.noreply.github.com>
2024-11-18 15:05:17 +0100

    Update recommended_movies.py

--- recommended_movies.py
+++ recommended_movies.py
@@ -13,21 +13,16 @@ sample_user_recommendations = {
         0: [592230, 25598, 840, 426249, 52109, 450487],
         3: [866413, 14456, 215, 91979, 74725, 283995],
     },
-    3: {
-        2: [11246, 2289, 32008, 14638, 9306, 364708],
-        4: [393561, 502425, 257440, 11795, 759054, 814800],
-    }
 }
 
 def get_recommended_movies(user_id):
     recommendations = sample_user_recommendations.get(int(user_id), {})
 
-    cluster_recommendations = {}
-    for cluster, movie_ids in recommendations.items():
-        movie_details = movies_df[movies_df['id'].isin(movie_ids)][['title', 'genres', 'vote_average']]
-        cluster_recommendations[cluster] = movie_details.to_dict(orient='records')
+    movie_ids = [movie_id for cluster in recommendations.values() for movie_id in cluster]
+
+    movie_details = movies_df[movies_df['id'].isin(movie_ids)][['title', 'vote_average']]
     
-    return cluster_recommendations
+    return movie_details.to_dict(orient='records')
 
 def show():
     st.header("Recommended Movies for You")
@@ -36,9 +31,8 @@ def show():
     if user_id:
         recommendations = get_recommended_movies(user_id)
         if recommendations:
-            for cluster, movies in recommendations.items():
-                st.subheader(f"Cluster {cluster}")
-                st.write(movies)  # Display each cluster's list of movies
+            for movie in recommendations:
+                st.write(f"{movie['title']} (Rating: {movie['vote_average']})")
         else:
             st.write("No recommendations found for this user.")
     else:
