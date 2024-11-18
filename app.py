b46e00747bf8f1d739caecc740371b2b1d56566b
devCsaba <134238949+devCsaba@users.noreply.github.com>
2024-11-18 15:11:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import recommended_movies
 import user_history
 import movie_search
@@ -6,14 +7,22 @@ import analysis
 
 st.title("Movie Recommendation System")
 
+user_history_df = pd.read_csv("users_history_v2.csv")
+unique_user_ids = sorted(user_history_df['user_id'].unique())  # Get unique user IDs
+
 if "user_id" not in st.session_state:
     st.session_state["user_id"] = None
 
 st.sidebar.title("User Selection")
-user_id = st.sidebar.text_input("Enter your User ID:")
 
-if user_id:
-    st.session_state["user_id"] = user_id
+selected_id = st.sidebar.selectbox("Select a User ID:", options=["Select a User ID"] + list(unique_user_ids), index=0)
+
+user_id_input = st.sidebar.text_input("Or enter your User ID:")
+
+if selected_id != "Select a User ID":
+    st.session_state["user_id"] = selected_id
+elif user_id_input:
+    st.session_state["user_id"] = user_id_input
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Home", "Recommended Movies", "Your History", "Movie Search", "Analysis"])
@@ -23,7 +32,7 @@ if page == "Home":
     if st.session_state["user_id"]:
         st.write(f"Currently selected User ID: {st.session_state['user_id']}")
     else:
-        st.write("Please enter your User ID in the sidebar to get personalized data.")
+        st.write("Please enter or select your User ID in the sidebar to get personalized data.")
 
 elif page == "Recommended Movies":
     recommended_movies.show()
